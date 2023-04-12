82eb1e7b1e024b601781c506f990f6693ddbbe48
priyansh masiyava <110883810+Whitedevil0410@users.noreply.github.com>
2023-04-12 22:06:35 +0530

    Add files via upload

---
+++ Patten.java
@@ -0,0 +1,10 @@
+class Patten {
+	public static void main(String[] args) {
+		for(int i=1;i<=7;i++){
+			for(int j=1;j<=i;j++){
+				System.out.println(j+" ");
+			}
+				System.out.println(" ");
+		}
+	}
+}
